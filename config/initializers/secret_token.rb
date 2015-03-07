a0156c70cf93c6da9895e526f02dec93bf1c4afd
Nick Hebner <hebnern@gmail.com>
2015-03-07 11:27:40 -0800

    Base brewbit-web project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrewbitWeb::Application.config.secret_key_base = 'c726f582ccd240de1dd1c226993a79544cf69c47ba47e5f8ede621df0949ccc7960b3b8e851c2a7192ae43abd1f0bf2d6c182ae963241ab3c4eb7e7b9dcd8985'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BrewbitWeb::Application.config.session_store :cookie_store, key: '_brewbit-web_session'
